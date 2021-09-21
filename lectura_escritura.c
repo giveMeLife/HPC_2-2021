becae8500eae0b57bcc096d483dee2a416cec035
Taekleee <catalina.morales.r@usach.cl>
2021-09-21 19:28:09 -0300

    funciones base

--- lectura_escritura.c
+++ lectura_escritura.c
@@ -2,14 +2,18 @@
 #include<emmintrin.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 #define SIZE 65536
 
 //abres la imagen y la guarda en buffer_out.
-void read_image(char* file_name, int * buffer_out, int size){
+void read_image(char* file_name, int * buffer_out, int M, int N){
     int descriptor;
     size_t r;
-    size = size*size;
+    int size = N*M;
     descriptor = open(file_name, O_RDONLY);
     if( descriptor == -1 ){
         fprintf(stderr,"Error abriendo imagen: %s\n",file_name);
@@ -25,10 +29,10 @@ void read_image(char* file_name, int * buffer_out, int size){
 }
 
 //transforma el arreglo (ya que la imagen al final es un arreglo de una dimension) a una matrix x,y de tamano size,size.
-void raw_to_matrix(int* buffer_in, int** matrix, int size){
+void raw_to_matrix(int* buffer_in, int** matrix, int M, int N){
     int k = 0;
-    for(int i = 0; i<size; i++){
-        for(int j = 0; j<size; j++){
+    for(int i = 0; i<M; i++){
+        for(int j = 0; j<N; j++){
             matrix[i][j] = buffer_in[k];
             k++;
         }
@@ -36,10 +40,10 @@ void raw_to_matrix(int* buffer_in, int** matrix, int size){
 }
 
 //transforma una matriz en un arreglo unidimensional, el cual es buffer_out. Tiene que ser de tamano n elevado a 2, o size elevado a dos.
-void matrix_to_raw(int* buffer_out, int** matrix, int size){
+void matrix_to_raw(int* buffer_out, int** matrix, int size1, int size2){
     int k = 0;
-    for(int i = 0; i<size; i++){
-        for(int j = 0; j<size; j++){
+    for(int i = 0; i<size1; i++){
+        for(int j = 0; j<size2; j++){
             buffer_out[k] = matrix[i][j];
             k++;
         }
@@ -47,10 +51,10 @@ void matrix_to_raw(int* buffer_out, int** matrix, int size){
 }
 
 //escribe la imagen. Se debe ingresar el arreglo raw, el cual es buffer_in.
-void write_image(char* file_name, int* buffer_in, int size){
+void write_image(char* file_name, int* buffer_in, int size1, int size2){
     int descriptor;
     size_t r;
-    size = size*size;
+    int size = size1*size2;
     descriptor = open(file_name, O_WRONLY | O_CREAT);
     if( descriptor == -1 ){
         fprintf(stderr,"Error creando imagen: %s\n",file_name);
@@ -65,22 +69,124 @@ void write_image(char* file_name, int* buffer_in, int size){
 
 }
 
-int main(void){
-    char filename[] = "simplehough1-256x256.raw";
-    int n = 256;
-    int* buffer = (int*)malloc(sizeof(int)*n*n);
-
-    read_image(filename, buffer, n);
-    int** matrix = (int**)malloc(sizeof(int*)*n);
-    for(int i = 0; i < n; i++){
-        matrix[i] = (int*)malloc(sizeof(int)*n);
+void recorrer(int ** matrix, int M, int N, int T, double dTeta, int ** H, double dR){
+  for (int x = 0; x < M; ++x){
+    for (int y = 0; y < N; ++y){
+      if (matrix[x][y] != 0){
+        for (int i = 0; i < T; i ++){
+          double r = (x* cos(i*dTeta*M_PI/180) + y* sin(i*dTeta*M_PI/180));
+          int r2 = r/(2*dR);
+          H[i][r2] =  H[i][r2] + 1;
+        }
+        
+      }
     }
-    raw_to_matrix(buffer, matrix, n);
+  }
+}
 
-    int* buffer2 = (int*)malloc(sizeof(int)*n*n);
-    matrix_to_raw(buffer2, matrix, n);
 
-    char fileout[] = "testfile.raw";
-    write_image(fileout, buffer2, n);
+int ** matrix_hugh(int M, int R, int ** H){
+  for (int i = 0; i < M; ++i){
+    for (int j = 0; j < R; ++j){
+      H[i][j] = 0;
+  }
+    }
+    return H;
+
+}
+
+void umbral(int ** H, int M, int R, int U){
+    for (int i = 0; i < M; ++i)
+    {
+       for (int j = 0; j < R; ++j)
+       {
+           if(H[i][j] > U){
+               H[i][j] = 255;
+           }
+           else{
+              H[i][j] = 0;
+
+        }
+       }
+    }
+    }
+
+
+int main(int argc, char *argv[]){
+    int c;
+  int N = 0;
+  int T = 0;
+  int R = 0;
+  int U = 0;
+  int M;
+  char * inputImg = malloc(sizeof(char)*30);
+  char * outputImg = malloc(sizeof(char)*30);
+
+  while ((c = getopt(argc, argv, "i:o:M:N:T:R:U:")) != -1)
+    switch (c)
+      {
+      case 'i':
+        strcpy(inputImg, optarg);
+        break;
+      case 'o':
+        strcpy(outputImg, optarg);
+        break;
+      case 'M':
+        M = atof(optarg);
+        break;
+      case 'N':
+        N = atof(optarg);
+        break;
+      case 'T':
+        T = atof(optarg);
+        break;
+      case 'R':
+        R = atof(optarg);
+        break;
+      case 'U':
+        U = atof(optarg);
+        break;
+      case '?':
+        if (optopt == 'c')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+    double dTeta = (M_PI/180)/T;
+    double dR = (N*sqrt(2))/(2*R);
+
+    //char filename[] = "simplehough1-256x256.raw";
+    int* buffer = (int*)malloc(sizeof(int)*M*N);
+
+    read_image(inputImg, buffer, M,N);
+    int** matrix = (int**)malloc(sizeof(int*)*M);
+    for(int i = 0; i < M; i++){
+        matrix[i] = (int*)malloc(sizeof(int)*N);
+    }
+    raw_to_matrix(buffer, matrix, M,N);
+
+    int** H = (int**)malloc(sizeof(int*)*T);
+    for(int i = 0; i < T; i++){
+        H[i] = (int*)malloc(sizeof(int)*R);
+    
+        }
+    H = matrix_hugh(T,R, H);  
+    recorrer(matrix,M, N, T,  dTeta,H, dR);
+
+    umbral(H,T,R,U);
+    
+    int* buffer2 = (int*)malloc(sizeof(int)*T*R);
+    matrix_to_raw(buffer2, H, T,R);
+
+    //char fileout[] = "b.raw";
+    write_image(outputImg, buffer2, T,R);
+    
     
 }
