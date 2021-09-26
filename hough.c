617af36668a9087cd99c734fb9bc4dda48694aa8
Taekleee <catalina.morales.r@usach.cl>
2021-09-26 12:19:18 -0300

    comentarios

--- hough.c
+++ hough.c
@@ -10,7 +10,12 @@
 #include <math.h>
 #define SIZE 65536
 
-//abres la imagen y la guarda en buffer_out.
+/*
+  Entrada: Nombre del archivo, buffer en donde se almacenará la imagen,
+           largo y ancho de la imagen.
+  Proceso: Se recorre el archivo y se almacenan los datos contenidos
+  Salida:  Arreglo que contiene los datos del archivo.raw
+*/
 void read_image(char* file_name, int * buffer_out, int M, int N){
     int descriptor;
     int size = N*M;
@@ -27,8 +32,12 @@ void read_image(char* file_name, int * buffer_out, int M, int N){
 
 
 }
-
-//transforma el arreglo (ya que la imagen al final es un arreglo de una dimension) a una matrix x,y de tamano size,size.
+/* Entrada: Buffer (arreglo) con la imagen.raw, matriz en donde se almacenarán los datos,
+            largo y ancho de la imagen.
+   Proceso: Transforma el arreglo (ya que la imagen al final es un arreglo de una dimension) 
+            a una matrix x,y de tamano size,size.
+   Salida:  Matriz de MxN con los datos almacenados
+*/
 void raw_to_matrix(int* buffer_in, int** matrix, int M, int N){
     int k = 0;
     for(int i = 0; i<M; i++){
@@ -39,7 +48,11 @@ void raw_to_matrix(int* buffer_in, int** matrix, int M, int N){
     }
 }
 
-//transforma una matriz en un arreglo unidimensional, el cual es buffer_out. Tiene que ser de tamano n elevado a 2, o size elevado a dos.
+/* Entrada: Arreglo en donde se almacenará la matriz H, matriz H, alto y ancho de la matriz. 
+   Proceso: Transforma una matriz en un arreglo unidimensional, el cual es buffer_out.
+            Tiene que ser de tamano MxN.
+   Salida:  Arreglo unidimensional con la matriz H.
+*/
 void matrix_to_raw(int* buffer_out, int** matrix, int size1, int size2){
     int k = 0;
     for(int i = 0; i<size1; i++){
@@ -50,7 +63,10 @@ void matrix_to_raw(int* buffer_out, int** matrix, int size1, int size2){
     }
 }
 
-//escribe la imagen. Se debe ingresar el arreglo raw, el cual es buffer_in.
+/* Entrada: Nombre del archivo de salida, arreglo unidimensional con la matriz H, Alto y ancho de la matriz.
+   Proceso: Se transforma la matriz H en un arreglo para que sea escrito como un archivo .raw
+   Salida:  Imagen .raw creada
+*/
 void write_image(char* file_name, int* buffer_in, int size1, int size2){
     int descriptor;
     int size = size1*size2;
@@ -68,6 +84,13 @@ void write_image(char* file_name, int* buffer_in, int size1, int size2){
 
 }
 
+/* Entrada: Matriz H, posición i del ángulo, valor obtenido del radio, delta del radio
+            N° de radios a calcular.
+   Proceso: Se parametriza el valor de r y se agrega a la matriz H. Si el valor de r
+            de la fórmula x*cos(angulo) + y*sen(angulo) es negativo, se calcula el valor
+            absoluto. A continuación se suma en 1 a la posición del ángulo y radio respectivo.
+   Salida:  Matriz H modificada con una nueva línea encontrada con angulo i y radio normalized_r
+*/
 void hough_vote(int ** hough_matrix, int i, int r, double dR, int R){
   int normalized_r;
   if(r < 0){
@@ -80,7 +103,14 @@ void hough_vote(int ** hough_matrix, int i, int r, double dR, int R){
 }
 
 
-void hough_algorithm(int ** matrix, int M, int N, int T, double dTeta, int ** H, double dR, double diagonal, double R){
+/* Entrada: Matriz con la imagen, Ancho y alto de la matriz, N° total de ángulos, delta del ángulo, 
+            Matriz H, delta del radio y N° total de radios.
+   Proceso: Se recorre la imagen pixel a pixel y en caso de que el valor de estos sea distinto de 0
+            se calcula para cada ángulo (según el N° de ángulos indicados) el valor del radio. Luego
+            se llama a la función hough_vote para parametrizar y aumentar en 1 a la matriz H.
+   Salida:  Matriz H modificada.
+*/
+void hough_algorithm(int ** matrix, int M, int N, int T, double dTeta, int ** H, double dR, double R){
   for (int x = 0; x < M; x++){
     for (int y = 0; y < N; y++){
       if (matrix[x][y] != 0){
@@ -95,6 +125,10 @@ void hough_algorithm(int ** matrix, int M, int N, int T, double dTeta, int ** H,
 }
 
 
+/* Entrada: Ancho y largo de la matriz H a crear y la matriz vacía.
+   Proceso: Se inicializa la matriz H en 0.
+   Salida:  Matriz H de ceros.
+*/
 int ** matrix_hough(int M, int R, int ** H){
   for (int i = 0; i < M; ++i){
     for (int j = 0; j < R; ++j){
@@ -105,6 +139,11 @@ int ** matrix_hough(int M, int R, int ** H){
 
 }
 
+/* Entrada: Matriz H, largo y ancho, valor umbral
+   Proceso: Se recorre la matriz H y en caso de que el valor del pixel sea mayor a la umbralización se establece
+            como 255, en caso contrario toma el valor 0.
+   Salida:  Matriz H umbralizada con 0 y 255.
+*/
 void umbral(int ** H, int M, int R, int U){
     for (int i = 0; i < M; ++i)
     {
@@ -121,7 +160,14 @@ void umbral(int ** H, int M, int R, int U){
     }
 }
 
-void parallel_hough_algorithm(int ** matrix, int M, int N, int T, float dTeta, int ** H, float dR, float* angles, float diagonal, float R){
+/* Entrada: Matriz que contiene la imagen, ancho y alto de la imagen, delta del ángulo, matriz H, delta del radio, arreglo de 
+            ángulos y N° de radios.
+   Proceso: Se asigna el valor de dTeta y 1/dR a dos registros distintos de 4 elementos, lo mismo con los valor de x e y. Luego
+            se realiza la ecuación con registro de a 4 de manera paralela. Finalmente se almacenan los resultados en la matriz
+            H.
+   Salida:  Matriz H modificada.
+*/
+void parallel_hough_algorithm(int ** matrix, int M, int N, int T, float dTeta, int ** H, float dR, float* angles, float R){
    __m128 dTeta2 = _mm_set1_ps(dTeta);
    __m128 dR2 = _mm_set1_ps(1/dR);
    float normalized_radius_att[4] __attribute__((aligned(16))) = { 0.0, 0.0, 0.0, 0.0 };
@@ -224,7 +270,7 @@ int main(int argc, char *argv[]){
 
     //Ejecución algoritmo secuencial
     start_t = clock();
-    hough_algorithm(matrix,M, N, T, dTeta,H, dR, diagonal, R);
+    hough_algorithm(matrix,M, N, T, dTeta,H, dR, R);
     end_t = clock();
     total_time = (double)(end_t - start_t) / CLOCKS_PER_SEC;
     printf("Tiempo de latencia parte secuencial: %lf\n", total_time  );
@@ -237,7 +283,7 @@ int main(int argc, char *argv[]){
 
     //Ejecución algoritmo paralelo
     start_t = clock();
-    parallel_hough_algorithm(matrix, M, N, T, (float)dTeta, H, (float)dR, angles, diagonal, R);
+    parallel_hough_algorithm(matrix, M, N, T, (float)dTeta, H, (float)dR, angles, R);
     end_t = clock();
     total_time = (double)(end_t - start_t) / CLOCKS_PER_SEC;
     printf("Tiempo de latencia parte paralela: %lf\n", total_time  );
